5db0f612c467c47c624f7dd4c0849c5a4e3b92a6
1 <144205582+ialeva@users.noreply.github.com>
2023-10-12 15:49:47 +0300

    Create Program.cs

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите число:");
+            //int n=Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //while(n!=0)
+            // {
+            //     n = n / 10;
+            //     count++;
+            // }
+            //Console.WriteLine($"{count}");
+
+            //Задача 1
+            // Соответствие номера из таблицы Unicode символа(каждый символ Unicode имеет свой порядковый номер).
+            //for(int i=0;i<256;i++)
+            //{
+            //    Console.WriteLine($"Код {i} соответствует символу {(char)i}");
+            //}
+
+            //Задача 2
+            //Console.Write("Введите символ:");
+            //char ch=char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символ {ch} имеет код {(int)ch}");
+
+            //Задача 3
+            //Console.Write("Введите символ:");
+            //char ch=char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символ {(char)(int)ch+1}");
+
+            //Задача 4
+            //Console.Write("Введите символ:");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символы:{(char)(int)ch-1};{(char)(int)ch-2}");
+
+            //Задача 5
+            //Console.Write("Введите символ:");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символы:{(char)(int)(ch - 1)};{(char)(int)(ch+1)}");
+
+            //Задача 6
+            //Console.Write("Введите название клуба:");
+            //string ch=Console.ReadLine();
+            //char[] omas = ch.ToCharArray();
+            //for(int i=0;i<omas.Length;i++)
+            //{
+            //    Console.WriteLine($"{omas[i]}");
+            //}
+            //foreach(char elem in omas)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+
+            //Задача 7
+            //Console.Write("Слово:");
+            //string ch = Console.ReadLine();
+            //char[] omas = ch.ToCharArray();
+            //for(int i=omas.Length-1;i>=0;i--)
+            //{
+            //    Console.WriteLine($"{omas[i]}");
+            //}
+
+            //
+            //Console.Write("Слово:");
+            //string ch = Console.ReadLine();
+            //Console.WriteLine($"{ch.ToUpper()}{ch.ToLower()}");
+            Console.ReadKey();
+        }
+    }
+}
